b4804a7c0035aa694890124e49088f45f0d97987
Christopher Styles <christopherstyles@gmail.com>
2016-10-03 16:21:48 -0700

    Do not raise exceptions on skip_before_action

--- app/controllers/transmuxer/notifications_controller.rb
+++ app/controllers/transmuxer/notifications_controller.rb
@@ -1,6 +1,6 @@
 module Transmuxer
   class NotificationsController < ApplicationController
-    skip_before_filter :verify_authenticity_token
+    skip_before_action :verify_authenticity_token, raise: false
 
     def create
       if resource && resource.zencoder_job_id == params[:job][:id]
